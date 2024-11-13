e585b653e3aac519a85385583530655d160fa64f
Martin Price <martin@systemhorizons.co.uk>
2024-11-13 18:02:00 +0000

    Stripped path

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,7 +5,7 @@ export default {
   title: 'Test Wiki Version',
   description: 'Just playing around.',
   base: "/test-wiki-version-control/",
-  head: [['link', { rel: 'icon', href: '/test-wiki-version-control/favicon.ico' }]]
+  head: [['link', { rel: 'icon', href: 'favicon.ico' }]]
   // themeConfig: {
   //   Type is `DefaultTheme.Config`
   // }
