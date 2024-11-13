eb095dff8ba32bbd3f998f4a4ceec76e199488af
Martin Price <martin@systemhorizons.co.uk>
2024-11-13 15:18:22 +0000

    Test after advice

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  // site-level options
+  title: 'VitePress',
+  description: 'Just playing around.',
+  base: "/test-wiki-version-control/",
+
+  // themeConfig: {
+  //   Type is `DefaultTheme.Config`
+  // }
+});
