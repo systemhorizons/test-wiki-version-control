3bd65dbe6dbc4789cfe6bb4e1d58adf97ea98b65
Martin Price <martin@systemhorizons.co.uk>
2024-11-13 18:56:30 +0000

    Fix config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   title: 'Test Wiki Version',
   description: 'Just playing around.',
   base: "/test-wiki-version-control/",
-  head: [['link', { rel: 'icon', href: '/test-wiki-version-control/favicon.ico' }]]
+  head: [['link', { rel: 'icon', href: '/test-wiki-version-control/favicon.ico' }]],
   themeConfig: {
     search: {
       provider: 'local'
