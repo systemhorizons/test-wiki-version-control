c51d70ab821283c268902da2816935542c87a00b
Martin Price <martin@systemhorizons.co.uk>
2024-11-13 12:00:20 +0000

    add config

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+// .vitepress/config.js
+export default {
+    // site-level options
+    title: 'Test wiki',
+    description: 'Just playing around.',
+  
+    themeConfig: {
+      // theme-level options
+    }
+  }
